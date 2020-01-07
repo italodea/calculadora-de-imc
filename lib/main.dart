dd7503338040bc72468052a96abab54259ec944a
italo de azevedo albuquerque <italodeazevedo99@gmail.com>
2020-01-07 01:46:20 -0300

    Adição dos inputs com o peso e a altura

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,27 @@ class _State extends State<Home> {
           IconButton(icon: Icon(Icons.refresh),onPressed: (){},)
         ],
       ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Icon(Icons.person_outline,size: 120.0,color: Colors.blueAccent,),
+          TextField(keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+            labelText: "Peso em Kg:",
+            labelStyle: TextStyle(color: Colors.blueAccent),
+          ),
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.blueAccent, fontSize: 25.0),),
+          TextField(keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Altura em cm:",
+              labelStyle: TextStyle(color: Colors.blueAccent),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.blueAccent, fontSize: 25.0),)
+        ],
+      ),
     );
   }
 }
