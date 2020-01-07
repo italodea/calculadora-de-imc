5b9596340fb16092ce5d2ba4e9152b83bf9e194c
italo de azevedo albuquerque <italodeazevedo99@gmail.com>
2020-01-07 02:04:31 -0300

    Adição dos inputs com o peso e a altura

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-void main(){
+
+void main() {
   runApp(MaterialApp(
     home: Home(),
   ));
@@ -14,35 +15,67 @@ class _State extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Calculadora de IMC"),
-        centerTitle: true,
-        backgroundColor: Colors.blueAccent,
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.refresh),onPressed: (){},)
-        ],
-      ),
-      backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Icon(Icons.person_outline,size: 120.0,color: Colors.blueAccent,),
-          TextField(keyboardType: TextInputType.number,
-          decoration: InputDecoration(
-            labelText: "Peso em Kg:",
-            labelStyle: TextStyle(color: Colors.blueAccent),
+        appBar: AppBar(
+          title: Text("Calculadora de IMC"),
+          centerTitle: true,
+          backgroundColor: Colors.blueAccent,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {},
+            )
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(10.0, 2.0, 10.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(
+                Icons.fitness_center,
+                size: 120.0,
+                color: Colors.blueAccent,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Peso em Kg:",
+                  labelStyle: TextStyle(color: Colors.blueAccent),
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.blueAccent, fontSize: 25.0),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Altura em cm:",
+                  labelStyle: TextStyle(color: Colors.blueAccent),
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.blueAccent, fontSize: 25.0),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                child: Container(
+                  height: 50.0,
+                  child: RaisedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Calcular",
+                      style: TextStyle(color: Colors.white, fontSize: 26.0),
+                    ),
+                    color: Colors.blueAccent,
+                  ),
+                ),
+              ),
+              Text(
+                "Info",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.blueAccent, fontSize: 25.0),
+              )
+            ],
           ),
-          textAlign: TextAlign.center,
-          style: TextStyle(color: Colors.blueAccent, fontSize: 25.0),),
-          TextField(keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: "Altura em cm:",
-              labelStyle: TextStyle(color: Colors.blueAccent),
-            ),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.blueAccent, fontSize: 25.0),)
-        ],
-      ),
-    );
+        ),);
   }
 }
